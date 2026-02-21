31a4aa4ce9c2ef8555ece3d87a971ed2595420a1
nachman <nachman.molko@gmail.com>
2026-02-21 16:56:22 +0200

    The whole workflow workedgit add .

--- Ansible/scripts/init-cluster.sh
+++ Ansible/scripts/init-cluster.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-git clone git@github.com:NachmanM/k8s-project.git /k8s-project
+if [ ! -d "/k8s-project" ]; then
+    git clone -c core.sshCommand="ssh -o StrictHostKeyChecking=no" git@github.com:NachmanM/k8s-project.git /k8s-project
+else
+    echo "Directory /k8s-project already exists. Skipping clone."
+fi
 
 
 kubectl create secret docker-registry regcred \
 --docker-server=992382545251.dkr.ecr.us-east-1.amazonaws.com \
 --docker-username=AWS \
 --docker-password=$(aws ecr get-login-password --region us-east-1) \
---namespace=default
+--namespace=default \
+--dry-run=client -o yaml | kubectl apply -f -
 
 
 helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver
@@ -19,4 +24,8 @@ helm upgrade --install aws-ebs-csi-driver \
   --namespace kube-system \
   --version 2.55.1
 
-  kubectl apply -R -f /k8s-project/k8s/
+kubectl apply -f /k8s-project/k8s/ingress-nginx/NameSpace.yaml
+# Wait until the namespace is actually ready
+kubectl wait --for=condition=Active namespace/ingress-nginx --timeout=3s
+
+kubectl apply -R -f /k8s-project/k8s/
--- apply.sh
+++ apply.sh
@@ -2,5 +2,6 @@
 
 terraform -chdir=./Terraform apply -auto-approve
 
-source Ansible/venv/bin/activate
-ansible-playbook Ansible/main.yaml
+source ./Ansible/venv/bin/activate
+export ANSIBLE_CONFIG=./Ansible/ansible.cfg
+ansible-playbook -i ./Ansible/ec2_inventory.aws_ec2.yaml ./Ansible/main.yaml
